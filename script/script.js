1be7d9d72a2106c9fa9cfdb4f07181ab5cb21945
RhysJosmin <55994011+Rhysjosmin@users.noreply.github.com>
2023-11-11 12:21:34 +0530

    Revert "V2"

---
+++ script/script.js
@@ -0,0 +1,38 @@
+$('.brand-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    autoplay:true,
+    responsive:{
+      0:{
+        items:1
+      },
+      600:{
+        items:3
+      },
+      1000:{
+        items:5
+      }
+    }
+  }) 
+  
+
+  function OpenReadMore(a){
+    const ReadMore=document.getElementById(a)
+    ReadMore.style.display='block';
+    ReadMore.style.top='2rem'
+    document.querySelector("body").style.overflow='hidden';
+
+    document.getElementById("main").style.transition='filter .6s'
+    document.getElementById("main").style.filter='blur(2px)';
+
+    
+  }
+
+  function CloseReadMore(a){
+    const ReadMore=document.getElementById(a)
+    ReadMore.style.top='60rem' 
+    //ReadMore.style.display='none';
+    document.querySelector("body").style.overflow='auto';
+    document.getElementById("main").style.filter='none';
+  }
+
